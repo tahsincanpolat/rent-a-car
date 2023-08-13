178033bfc86c8fd9dfe85def3a99422b8c426b87
tahsincanpolat <tahsincanpolat@gmail.com>
2023-08-13 10:21:03 +0300

    login formu boyutu ayarlandı

--- rentACar/RentACar/Form1.Designer.cs
+++ rentACar/RentACar/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(614, 372);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
